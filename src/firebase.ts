b81349e8405a3d2eb5b2c69fc844107f64bf8422
a96363877 <a96363877@gmail.com>
2025-04-09 15:58:33 +0300

    wef

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,14 @@ import { doc, getFirestore, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   
-   apiKey: "AIzaSyBoqC6S8FvbWhud_PnD6IC4pF61iffe4tI",
-  authDomain: "qatar22-d0e6a.firebaseapp.com",
-  projectId: "qatar22-d0e6a",
-  storageBucket: "qatar22-d0e6a.firebasestorage.app",
-  messagingSenderId: "349776944758",
-  appId: "1:349776944758:web:6a8dd2558e8ac248570957",
-  measurementId: "G-079WEHH5KL"
+  apiKey: "AIzaSyBTRzIOu6yN0Fiq6HGgtam6xc9MIsGKiE4",
+  authDomain: "tree-sa.firebaseapp.com",
+  databaseURL: "https://tree-sa-default-rtdb.firebaseio.com",
+  projectId: "tree-sa",
+  storageBucket: "tree-sa.firebasestorage.app",
+  messagingSenderId: "1028453914907",
+  appId: "1:1028453914907:web:31fd99fe3dcdb3b6fc5bbe",
+  measurementId: "G-9QR56T5899"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
