5080013c7a553640eb7c28a6f8a078d30a9e599f
a96363877 <a96363877@gmail.com>
2025-06-26 08:57:07 +0300

    sss

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,14 +4,14 @@ import { doc, getFirestore, setDoc } from 'firebase/firestore';
 import { getDatabase} from 'firebase/database';
 
 const firebaseConfig = {
-  
-  apiKey: "AIzaSyDqUCm3TP3YQ5stW9mWLACTiW6WowQMrfE",
-  authDomain: "swed-8d73b.firebaseapp.com",
-  projectId: "swed-8d73b",
-  storageBucket: "swed-8d73b.firebasestorage.app",
-  messagingSenderId: "685168570681",
-  appId: "1:685168570681:web:b2f367991b4f57c4ec38e8",
-  measurementId: "G-RW2J2Z9PXD"
+  apiKey: "AIzaSyC-fR9vsFJ0gyOcysg5SG5DMtATNzTyW1c",
+  authDomain: "exam-app-2ec1d.firebaseapp.com",
+  databaseURL: "https://exam-app-2ec1d-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "exam-app-2ec1d",
+  storageBucket: "exam-app-2ec1d.firebasestorage.app",
+  messagingSenderId: "116559499407",
+  appId: "1:116559499407:web:8c5ecf129f5c437e3ea0fa",
+  measurementId: "G-MY09H839C3"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
